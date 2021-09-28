878c57f32b7cef9b205526d5e10f4b203ca19a00
Decagon <favour.chukwuemeka@decagon.dev>
2021-09-28 18:21:18 +0100

    making slight changes

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -4,10 +4,8 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: 5, text: 'Open Source Projects'},
+  { number: 500, text: 'coding hours', },
 ];
 
 const Acomplishments = () => (
